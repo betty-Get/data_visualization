b66df8480f2cb9255bc17073b5038fe4fadff232
betty <betelhem.getachew2926@gmail.com>
2023-09-01 09:54:16 +0300

    data displayed on table

--- src/App.js
+++ src/App.js
@@ -20,73 +20,91 @@ function App() {
 
   return (
     <div className="App">
-      {console.log(data)}
-      <div className="w-[290px] sm:w-[550px] lg:w-[900px] xl:w-[1200px] mx-auto">
-        <div className="">
-          <table className="min-w-full overflow-x-scroll">
-            <thead className="bg-white border-b">
-              <tr>
-                <th
-                  scope="col"
-                  className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                >
-                  #
-                </th>
-                <th
-                  scope="col"
-                  className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                >
-                  Group Name
-                </th>
-                <th
-                  scope="col"
-                  className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                >
-                  Status
-                </th>
-                <th
-                  scope="col"
-                  className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                >
-                  email
-                </th>
-                <th
-                  scope="col"
-                  className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                >
-                  Idea
-                </th>
-                <th
-                  scope="col"
-                  className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                ></th>
-              </tr>
-            </thead>
-            {data ?
-             data.map((data) => (
-            <>
-              <tbody>
-                <tr
-                  key={1}
-                  className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
-                >
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{data.name}</td>
-                  <td
-                    key={2}
-                    className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                  ></td>
-                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"></td>
-                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"></td>
-                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"></td>
-                  <td>
-                    {/* <Link href={`/user/${user._id}`}>Detail</Link> */}
-                  </td>
-                </tr>
-              </tbody>
-            </>
-             )) : <p>No record to display</p>} 
-          </table>
-        </div>
+      <div className="w-[550px] lg:w-[900px] xl:w-[1200px] mx-auto">
+        <table className="min-w-full overflow-x-scroll">
+          <thead className="bg-white border-b">
+            <tr>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-4 py-4 text-left"
+              >
+                Id
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 py-4 text-left"
+              >
+                Name
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+              >
+                Rank
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+              >
+                Price(USD)
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+              >
+                Percent Change(24h)
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+              >
+                Price(BTC)
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+              >
+                Market Cap(USD)
+              </th>
+            </tr>
+          </thead>
+          {data ? (
+            data.map((data) => (
+              <>
+                <tbody>
+                  <tr
+                    key={data.id}
+                    className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+                  >
+                    <td className="px-4 py-4  text-sm font-medium text-gray-900">
+                      {data.id}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light py-4">
+                      {data.name}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-2 py-4 ">
+                      {data.rank}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-2 py-4 ">
+                      {data.price_usd}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light py-2 ">
+                      {data.percent_change_24h}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-2 py-4 ">
+                      {data.price_btc}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-2 py-4 ">
+                      {data.market_cap_usd}
+                    </td>
+                  </tr>
+                </tbody>
+              </>
+            ))
+          ) : (
+            <p>No record to display</p>
+          )}
+        </table>
       </div>
     </div>
   );
