c5068f604e5716cfd521eb0b6b6e411d1e85f8ba
@betty2926 <betelhem.getachew2926@gmail.com>
2023-09-02 22:44:31 -0700

    price_use sort added

--- src/component/table.js
+++ src/component/table.js
@@ -3,21 +3,32 @@ import { useState } from "react";
 import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 
 function Table({ results }) {
+  // const [att, setAtt] = useState(true);
+
   const [id, setId] = useState(true);
   const [name, setName] = useState(true);
   const [rank, setRank] = useState(true);
+  const [price, setPrice] = useState(true);
+  const [percent, setPercent] = useState(true);
+  const [priceBtc, setPriceBtc] = useState(true);
 
   // sort by id
-  const handleSortId = () => {
-    setId(!id);
-    // const keys = ["id", "rank"];
-    if (id === true) {
-      //   results.sort((a, b) =>
-      //     keys.some((key) => Number(a[key]) - Number(b[key]))
-      //   );
-      results.sort((a, b) => Number(a.id) - Number(b.id));
+  const handleSortId = (att, val) => {
+    if (val === "id") {
+      setId(!att);
+    } else if (val === "rank") {
+      setRank(!att);
+    } else if (val === "price_usd") {
+      setPrice(!att);
+    } else if (val === "percent_change_24h") {
+      setPercent(!att);
+    } else if (val === "price_btc") {
+      setPriceBtc(!att);
+    }
+    if (att === true) {
+      results.sort((a, b) => Number(a[val]) - Number(b[val]));
     } else {
-      results.sort((a, b) => Number(b.id) - Number(a.id));
+      results.sort((a, b) => Number(b[val]) - Number(a[val]));
     }
   };
 
@@ -31,13 +42,33 @@ function Table({ results }) {
     }
   };
 
+  //sort by rank
   const handleRankId = () => {
     setRank(!rank);
-
     if (rank === true) {
       results.sort((a, b) => Number(a.rank) - Number(b.rank));
     } else {
       results.sort((a, b) => Number(b.rank) - Number(a.rank));
+    }
+  };
+
+  //sort by price
+  const handlePriceId = () => {
+    setPrice(!price);
+    if (price === true) {
+      results.sort((a, b) => Number(a.price_usd) - Number(b.price_usd));
+    } else {
+      results.sort((a, b) => Number(b.price_usd) - Number(a.price_usd));
+    }
+  };
+
+  //sort by percent
+  const handlePercentId = () => {
+    setPercent(!percent);
+    if (percent === true) {
+      results.sort((a, b) => Number(a.price_usd) - Number(b.price_usd));
+    } else {
+      results.sort((a, b) => Number(b.price_usd) - Number(a.price_usd));
     }
   };
 
@@ -55,9 +86,13 @@ function Table({ results }) {
                   Id
                   <span className="pl-1 pt-1 text-blue-400">
                     {id ? (
-                      <AiOutlineArrowDown onClick={handleSortId} />
+                      <AiOutlineArrowDown
+                        onClick={() => handleSortId(id, "id")}
+                      />
                     ) : (
-                      <AiOutlineArrowUp onClick={handleSortId} />
+                      <AiOutlineArrowUp
+                        onClick={() => handleSortId(id, "id")}
+                      />
                     )}
                   </span>
                 </th>
@@ -84,9 +119,13 @@ function Table({ results }) {
                     Rank
                     <span className="pl-1 pt-1 text-blue-400">
                       {rank ? (
-                        <AiOutlineArrowDown onClick={handleRankId} />
+                        <AiOutlineArrowDown
+                          onClick={() => handleSortId(rank, "rank")}
+                        />
                       ) : (
-                        <AiOutlineArrowUp onClick={handleRankId} />
+                        <AiOutlineArrowUp
+                          onClick={() => handleSortId(rank, "rank")}
+                        />
                       )}
                     </span>
                   </span>
@@ -95,19 +134,60 @@ function Table({ results }) {
                   scope="col"
                   className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
                 >
-                  Price(USD)
+                  <span className="flex">
+                    Price(USD)
+                    <span className="pl-1 pt-1 text-blue-400">
+                      {price ? (
+                        <AiOutlineArrowDown
+                          onClick={() => handleSortId(price, "price_usd")}
+                        />
+                      ) : (
+                        <AiOutlineArrowUp
+                          onClick={() => handleSortId(price, "price_usd")}
+                        />
+                      )}
+                    </span>
+                  </span>
                 </th>
                 <th
                   scope="col"
                   className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
                 >
-                  Percent Change(24h)
+                  <span className="flex justify-center items-center">
+                    Percent Change(24h)
+                    <span className=" text-blue-400">
+                      {percent ? (
+                        <AiOutlineArrowDown
+                          onClick={() =>
+                            handleSortId(percent, "percent_change_24h")
+                          }
+                        />
+                      ) : (
+                        <AiOutlineArrowUp
+                          onClick={() =>
+                            handleSortId(percent, "percent_change_24h")
+                          }
+                        />
+                      )}
+                    </span>
+                  </span>
                 </th>
                 <th
                   scope="col"
                   className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
                 >
                   Price(BTC)
+                  <span className=" text-blue-400">
+                    {priceBtc ? (
+                      <AiOutlineArrowDown
+                        onClick={() => handleSortId(priceBtc, "price_btc")}
+                      />
+                    ) : (
+                      <AiOutlineArrowUp
+                        onClick={() => handleSortId(priceBtc, "price_btc")}
+                      />
+                    )}
+                  </span>
                 </th>
                 <th
                   scope="col"
